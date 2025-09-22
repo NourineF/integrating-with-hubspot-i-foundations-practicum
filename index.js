805075c71548051e833a31ca4b2f7a2d307cbf9e
Nourine Founouni <nourinefounouni@Nourines-MacBook-Air.local>
2025-09-22 17:48:10 -0500

    Finished practicum app: homepage + update form + HubSpot contacts

--- index.js
+++ index.js
@@ -1,71 +1,51 @@
-const express = require('express');
-const axios = require('axios');
+const express = require("express");
+const axios = require("axios");
+const path = require("path");
+require("dotenv").config();
+
 const app = express();
-
-app.set('view engine', 'pug');
-app.use(express.static(__dirname + '/public'));
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
 app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
 
-// * Please DO NOT INCLUDE the private app access token in your repo. Don't do this practicum in your normal account.
-const PRIVATE_APP_ACCESS = '';
-
-// TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
-
-// * Code for Route 1 goes here
-
-// TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
-
-// * Code for Route 2 goes here
-
-// TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
-
-// * Code for Route 3 goes here
-
-/** 
-* * This is sample code to give you a reference for how you should structure your calls. 
-
-* * App.get sample
-app.get('/contacts', async (req, res) => {
-    const contacts = 'https://api.hubspot.com/crm/v3/objects/contacts';
-    const headers = {
-        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
-        'Content-Type': 'application/json'
-    }
-    try {
-        const resp = await axios.get(contacts, { headers });
-        const data = resp.data.results;
-        res.render('contacts', { title: 'Contacts | HubSpot APIs', data });      
-    } catch (error) {
-        console.error(error);
-    }
+const HS = axios.create({
+  baseURL: "https://api.hubapi.com",
+  headers: { Authorization: `Bearer ${process.env.ACCESS_TOKEN}` }
 });
 
-* * App.post sample
-app.post('/update', async (req, res) => {
-    const update = {
-        properties: {
-            "favorite_book": req.body.newVal
-        }
-    }
-
-    const email = req.query.email;
-    const updateContact = `https://api.hubapi.com/crm/v3/objects/contacts/${email}?idProperty=email`;
-    const headers = {
-        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
-        'Content-Type': 'application/json'
-    };
-
-    try { 
-        await axios.patch(updateContact, update, { headers } );
-        res.redirect('back');
-    } catch(err) {
-        console.error(err);
-    }
-
+app.get("/", async (req, res) => {
+  try {
+    const r = await HS.get("/crm/v3/objects/contacts", {
+      params: { properties: "firstname,game_name,publisher,price", limit: 100 }
+    });
+    res.render("homepage", { title: "Contact Table with Games", contacts: r.data.results || [] });
+  } catch (_) {
+    res.status(500).send("Error loading contacts. Check token/scopes.");
+  }
 });
-*/
 
+app.get("/update-cobj", (req, res) => {
+  res.render("updates", { title: "Update Custom Object Form | Integrating With HubSpot I Practicum" });
+});
 
-// * Localhost
-app.listen(3000, () => console.log('Listening on http://localhost:3000'));
+app.post("/update-cobj", async (req, res) => {
+  const { name, game_name, publisher, price } = req.body;
+  try {
+    await HS.post("/crm/v3/objects/contacts", {
+      properties: {
+        firstname: name || "",
+        game_name: game_name || "",
+        publisher: publisher || "",
+        price: price ? Number(price) : null
+      }
+    });
+    res.redirect("/");
+  } catch (_) {
+    res.status(500).send("Create failed. Make sure the 3 contact properties exist.");
+  }
+});
+
+app.listen(process.env.PORT || 3000, () =>
+  console.log(`http://localhost:${process.env.PORT || 3000}`)
+);
+
